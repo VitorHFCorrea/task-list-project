2a9cfbef0add7b5d8222671eec58e9aad671667d
VitorHFCorrea <143048290+VitorHFCorrea@users.noreply.github.com>
2025-03-29 17:36:09 -0300

    theme color + placeholder changes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,10 @@
 function Input(props) {
   return (
     <input
-      className="border border-blue-400 outline-blue-400 px-4 py-2 rounded-md"
+      className='border border-sky-400 outline-sky-400 px-4 py-2 rounded-md placeholder:italic'
       {...props} //passa todas as props recebidas para o input
     />
-  );
+  )
 }
 
-export default Input;
+export default Input
